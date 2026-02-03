75ecfa335fd5bb5d21b9e1af11ac7398dede9c56
jesus-crispus <137449522+jesus-crispus@users.noreply.github.com>
2026-02-03 20:28:32 +0100

    works localy

--- multiplayer-test/player.gd
+++ multiplayer-test/player.gd
@@ -2,14 +2,16 @@ extends Node3D
 
 @export var speed : float
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	if !is_multiplayer_authority(): return
 	var input_dir = Input.get_vector("move_down", "move_up","move_left", "move_right")
 	input_dir = input_dir.normalized()
 	position.x += input_dir.x * speed * delta
 	position.z += input_dir.y * speed * delta
+	print("position", position)
